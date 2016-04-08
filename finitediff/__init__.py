8991051ed040546566b7a0bdc5e73fc4ab63920d
Björn Dahlgren <bjodah@gmail.com>
2016-04-08 20:27:07 +0200

    relase -> _release, update __init__.py wrt setup.cfg

--- finitediff/__init__.py
+++ finitediff/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .release import __version__
-assert __version__  # silence pyflakes
+from ._release import __version__
 
 try:
     from finitediff._finitediff_templated import (
@@ -13,7 +12,3 @@ except ImportError:
         derivatives_at_point_by_finite_diff, interpolate_by_finite_diff,
         get_weights
     )
-
-assert derivatives_at_point_by_finite_diff  # silence pyflakes
-assert interpolate_by_finite_diff  # silence pyflakes
-assert get_weights  # silence pyflakes
