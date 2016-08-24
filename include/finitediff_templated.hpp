b555957025d754e63c68e6a22392444300cd82c9
Björn Dahlgren <bjodah@gmail.com>
2016-08-24 16:28:25 +0200

    use unsigned instead of int where applicable

--- include/finitediff_templated.hpp
+++ include/finitediff_templated.hpp
@@ -31,14 +31,14 @@ namespace finitediff {
         c1 = 1;
         c4 = grid[0] - around;
         weights[0] = 1;
-        for (int i=1; i < len_g*(max_deriv+1); ++i)
+        for (unsigned i=1; i < len_g*(max_deriv+1); ++i)
             weights[i] = 0;  // clear weights
-        for (int i=1; i < len_g; ++i){
+        for (unsigned i=1; i < len_g; ++i){
             const int mn = (i < max_deriv) ? i : max_deriv; // min(i, max_deriv)
             Real_t c2 = 1;
             c5 = c4;
             c4 = grid[i] - around;
-            for (int j=0; j<i; ++j){
+            for (unsigned j=0; j<i; ++j){
                 const Real_t c3 = grid[i] - grid[j];
                 const Real_t c3_r = 1/c3;
                 c2 = c2*c3;
@@ -51,7 +51,7 @@ namespace finitediff {
                     }
                     weights[i] = -c1*c5*weights[i-1]*c2_r;
                 }
-                for (int k=mn; k>=1; --k){
+                for (unsigned k=mn; k>=1; --k){
                     const Real_t tmp1 = weights[j + k*len_g];
                     const Real_t tmp2 = weights[j + (k-1)*len_g];
                     weights[j + k*len_g] = (c4*tmp1 - k*tmp2)*c3_r;
@@ -84,28 +84,28 @@ namespace finitediff {
         Real_t c1, c4, c5;
         c1 = 1;
         c4 = x[0] - z;
-        for (int i=0; i < (nd+1)*(m+1); ++i)
+        for (unsigned i=0; i < (nd+1)*(m+1); ++i)
             c[i] = 0;
         c[0] = 1;
-        for (int i=1; i <= nd; ++i){
+        for (unsigned i=1; i <= nd; ++i){
             const int mn = (i < m) ? i : m; // min(i, m)
             Real_t c2 = 1;
             c5 = c4;
             c4 = x[i] - z;
-            for (int j=0; j<i; ++j){
+            for (unsigned j=0; j<i; ++j){
                 const Real_t c3 = x[i] - x[j];
                 const Real_t c3_r = 1/c3;
                 c2 = c2*c3;
                 if (j == i-1){
                     const Real_t c2_r = 1/c2;
-                    for (int k=mn; k>=1; --k){
+                    for (unsigned k=mn; k>=1; --k){
                         const Real_t tmp1 = c[i - 1 + (k-1)*(nd+1)];
                         const Real_t tmp2 = c[i - 1 + k*(nd+1)];
                         c[i + k*(nd+1)] = c1*(k*tmp1 - c5*tmp2)*c2_r;
                     }
                     c[i] = -c1*c5*c[i-1]*c2_r;
                 }
-                for (int k=mn; k>=1; --k){
+                for (unsigned k=mn; k>=1; --k){
                     const Real_t tmp1 = c[j + k*(nd+1)];
                     const Real_t tmp2 = c[j + (k-1)*(nd+1)];
                     c[j + k*(nd+1)] = (c4*tmp1 - k*tmp2)*c3_r;
