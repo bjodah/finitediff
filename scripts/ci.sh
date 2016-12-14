fbfd9085e61af4269f1779f92c734c4c10e19afe
Björn Dahlgren <bjodah@gmail.com>
2016-12-14 14:08:57 +0100

    Update setup.py and CI scripts

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -6,14 +6,14 @@ if [[ "$CI_BRANCH" =~ ^v[0-9]+.[0-9]?* ]]; then
     eval export ${PKG_NAME^^}_RELEASE_VERSION=\$CI_BRANCH
     echo ${CI_BRANCH} | tail -c +2 > __conda_version__.txt
 fi
-python2.7 setup.py sdist
-for PYTHON in python2.7 python3.4; do
+python2 setup.py sdist
+for PYTHON in python2 python3; do
     (cd dist/; $PYTHON -m pip install $PKG_NAME-$($PYTHON ../setup.py --version).tar.gz)
     (cd /; $PYTHON -m pytest --pyargs $PKG_NAME)
     $PYTHON -m pip install --user -e .[all]
 done
-PYTHONPATH=$(pwd) PYTHON=python2.7 ./scripts/run_tests.sh
-PYTHONPATH=$(pwd) PYTHON=python3.4 ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
+PYTHONPATH=$(pwd) PYTHON=python2 ./scripts/run_tests.sh
+PYTHONPATH=$(pwd) PYTHON=python3 ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
 
 # Make sure repo is pip installable from git-archive zip
