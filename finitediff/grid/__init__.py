642728081ec4120763bacdbb00cffc51a63b0642
Björn Dahlgren <bjodah@gmail.com>
2017-10-25 00:29:23 +0200

    Progress on rebalancing (not yet optimal)

--- finitediff/grid/__init__.py
+++ finitediff/grid/__init__.py
@@ -1,5 +1,5 @@
 from .rebalance import rebalanced_grid
 from .refine import refine_grid
 from .make import adapted_grid
-from .util import locate_discontinuity, pool_discontinuity_approx
+from .util import locate_discontinuity, pool_discontinuity_approx, grid_error
 from .plotting import plot_convergence
--- finitediff/grid/util.py
+++ finitediff/grid/util.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function)
+
+from ..util import interpolate_ahead
+
+
 def locate_discontinuity(grid, y, consider, trnsfm=lambda x: x, ntrail=2):
     y = np.asarray(y, dtype=np.float64)
     dy = np.diff(y)
@@ -17,3 +23,21 @@ def pool_discontinuity_approx(loc_res, consistency_criterion=10):
     points = np.array(loc_res)
     w1 = np.abs(points[:, 1])
     return avg_stddev(points[:, 0], w1)
+
+
+def grid_error(grid, y, ntrail=2):
+    """ Estimates error at each grid point from extrapolation.
+
+    Extrapolates from left ('fw') or from right ('bw').
+
+    Parameters
+    ----------
+    grid : array
+    y : array
+    ntrail : int
+        Number of points to include in the look-ahead extrapolation.
+
+    """
+    slcs, errs = [], []
+    est, _ = interpolate_ahead(grid, y, ntrail, direction='both')
+    return est - y
