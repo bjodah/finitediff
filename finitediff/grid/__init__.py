b3e8bd2f90f87c261db377ca1a658301a2dd28a1
Björn Dahlgren <bjodah@gmail.com>
2017-10-25 14:58:16 +0200

    Add grid_pruning_mask()

--- finitediff/grid/__init__.py
+++ finitediff/grid/__init__.py
@@ -1,4 +1,4 @@
-from .rebalance import rebalanced_grid
+from .rebalance import rebalanced_grid, grid_pruning_mask
 from .refine import refine_grid
 from .make import adapted_grid
 from .util import locate_discontinuity, pool_discontinuity_approx, grid_error
--- finitediff/grid/rebalance.py
+++ finitediff/grid/rebalance.py
@@ -12,6 +12,7 @@ def _avgdiff(x):
     dx2[1:-1] = 0.5*(dx[1:] + dx[:-1])
     return dx2
 
+
 def rebalanced_grid(grid, err, base=0.25, num=None, resolution_factor=10, smooth_fact=1.0):
     if num is None:
         num = grid.size
@@ -38,3 +39,39 @@ def rebalanced_grid(grid, err, base=0.25, num=None, resolution_factor=10, smooth
     interr = np.cumsum(smoothed * _avgdiff(finegrid))
     cb = interp1d(interr, finegrid)
     return cb(np.linspace(interr[0], interr[-1], num))
+
+
+def grid_pruning_mask(grid, err, ndrop=None, protect_sparse=None, pow_err=2, pow_dx=2):
+    """ Returns a mask for grid pruning.
+
+    Parameters
+    ----------
+    grid : array
+    err : array
+    ndrop : int
+        If not provided taken as 25% of grid size (rounded upward).
+    protect_sparse : int
+        If not provided taken as 25% of grid size (rounded upward).
+    pow_err : number
+        Exponent of error in weighting.
+    pow_dx : number
+        Exponent of grid spacing in weighting.
+    """
+    if ndrop is None:
+        ndrop = math.ceil(grid.size * 0.25)
+    if protect_sparse is None:
+        protect_sparse = math.ceil(grid.size * 0.25)
+    dx = _avgdiff(grid)
+    protected = np.argsort(dx)[-protect_sparse:]
+    score = err**pow_err * dx**pow_dx
+    importance = np.argsort(score)
+    drop = []
+    for considered in importance:
+        if considered in protected:
+            continue
+        if considered - 1 in drop or considered + 1 in drop:
+            continue
+        drop.append(considered)
+        if len(drop) == ndrop:
+            break
+    return ~np.in1d(np.arange(grid.size), drop)
