0c70203cc5ffa7a305fc287a57827e03b5912959
Björn Ingvar Dahlgren <bjodah@gmail.com>
2019-07-28 12:59:48 +0200

    clang-8

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,8 +7,8 @@ fi
 python3 setup.py sdist
 (cd dist/; python3 -m pip install $PKG_NAME-$(python3 ../setup.py --version).tar.gz)
 (cd /; python3 -m pytest --pyargs $PKG_NAME)
-CXX=clang++-6.0 CC=clang-6.0 CFLAGS='-fsanitize=address' python3 -m pip install --force-reinstall .[all]
-PYTHONPATH=$(pwd) ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
+CXX=clang++-8 CC=clang-8 CFLAGS='-fsanitize=address' python3 -m pip install --force-reinstall .[all]
+LD_PRELOAD=/usr/lib/llvm-8/lib/clang/8.0.1/lib/linux/libclang_rt.asan-x86_64.so PYTHONPATH=$(pwd) ./scripts/run_tests.sh --cov $PKG_NAME --cov-report html
 ./scripts/coverage_badge.py htmlcov/ htmlcov/coverage.svg
 
 # Make sure repo is pip installable from git-archive zip
