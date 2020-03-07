e322667d6dc5c4c0114d7c389b96966a17228c7b
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-03-07 22:08:27 +0100

    py3

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -11,7 +11,7 @@
 NARGS=$#
 PKG=$(find . -maxdepth 2 -name __init__.py -print0 | xargs -0 -n1 dirname | xargs basename)
 AUTHOR=$(head -n 1 AUTHORS)
-sphinx-apidoc --full --force -A "${AUTHOR/ö/oe}" --module-first --doc-version=$(python setup.py --version) -F -o doc $PKG/ $PKG/tests/
+sphinx-apidoc --full --force -A "${AUTHOR/ö/oe}" --module-first --doc-version=$(python3 setup.py --version) -F -o doc $PKG/ $PKG/tests/
 #sed -i 's/Contents/.. include:: ..\/README.rst\n\nContents/g' doc/index.rst
 cat <<EOF >>doc/index.rst
 
